bec625f77a3e3bed2785ee9059c89467a70c72b6
Angel Peltekov <ai.peltekov@gmail.com>
2018-01-26 04:06:28 +0200

    Add project files.

---
+++ PlebBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlebBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
