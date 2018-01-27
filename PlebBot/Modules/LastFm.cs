0cb2f895af6e9283fda7ab6dc83a7a24bf7a886d
Angel Peltekov <ai.peltekov@gmail.com>
2018-01-27 02:28:18 +0200

    set up bot log in and last.fm module

---
+++ PlebBot/Modules/LastFm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Discord;
+using IF.Lastfm.Core.Api;
+using Microsoft.Extensions.Configuration;
+
+namespace PlebBot.Modules
+{
+    [Group("fm")]
+    class LastFm : ModuleBase<SocketCommandContext>
+    {
+        private LastfmClient _client;
+
+        public LastFm()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("_config.json").Build();
+            this._client = new LastfmClient(config["tokens:lastfm_key"], config["tokens:lastfm_secret"]);
+        }
+
+        [Command]
+        [Summary("Show what you're listening to")]
+        public async Task Scrobble(string username = "")
+        {
+            var msg = new EmbedBuilder();
+
+            if (username != String.Empty)
+            {
+                var response = await _client.User.GetRecentScrobbles(username, null, 1, 2);
+                msg.WithTitle($"Recent tracks for {username}")
+                    .WithThumbnailUrl(response.Content[0].Images.Medium.ToString())
+                    .WithUrl($"https://www.last.fm/user/{username}")
+                    .AddField("**Current track:**",
+                        $"{response.Content[0].ArtistName} - {response.Content[0].Name} " +
+                        $"[{response.Content[0].AlbumName}]")
+                    .AddField("**Previous:**",
+                        $"{response.Content[1].ArtistName} - {response.Content[1].Name} " +
+                        $"[{response.Content[1].AlbumName}]")
+                    .WithColor(Color.DarkBlue);
+            }
+            else
+            {
+                msg.AddField("Error", "You must enter a username.")
+                    .WithColor(Color.DarkRed);
+            }
+
+            await Context.Channel.SendMessageAsync("", false, msg);
+        }
+    }
+}
--- PlebBot/Program.cs
+++ PlebBot/Program.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using PlebBot.Modules;
 
-namespace PlebBot
+namespace PMCDBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private CommandService _commands;
+        private DiscordSocketClient _client;
+        private IServiceProvider _services;
+        private IConfigurationRoot _config;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("_config.json");
+            _config = builder.Build();
+
+            _services = new ServiceCollection()
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    LogLevel = LogSeverity.Verbose,
+                    MessageCacheSize = 1000
+                }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    DefaultRunMode = RunMode.Async,
+                    LogLevel = LogSeverity.Verbose
+                }))
+                .AddSingleton<Random>()
+                .AddSingleton(_config)
+                .BuildServiceProvider();
+
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            _commands = new CommandService();
+            await InstallCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, _config["tokens:discord_token"]);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModuleAsync<InfoModule>();
+            await _commands.AddModuleAsync<LastFm>();
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            int argPos = 0;
+            if (!(message.HasStringPrefix(_config["prefix"], ref argPos) ||
+                  message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+            var context = new SocketCommandContext(_client, message);
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
+            {
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+            }
         }
     }
 }
