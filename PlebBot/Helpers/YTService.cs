b1fefff964a18f46f673aa916e31b3e8335eaa0b
Angel Peltekov <ai.peltekov@gmail.com>
2018-02-20 12:35:30 +0200

    better YT response format

--- PlebBot/Helpers/YTService.cs
+++ PlebBot/Helpers/YTService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using Discord.Commands;
 using Google.Apis.Services;
 using Google.Apis.YouTube.v3;
@@ -30,15 +32,23 @@ namespace PlebBot.Helpers
             var result = await GetSearchResultsAsync(query);
             if (result.Items.Count > 0)
             {
-                var videoId = result.Items.FirstOrDefault().Id.VideoId;
+                var videoId = result.Items.FirstOrDefault()?.Id.VideoId;
                 var video = await GetVideoAsync(videoId);
-                var videoDate = video.Snippet.PublishedAt.Value.ToString("MMMM dd, yyyy");
+                var videoDate = video.Snippet.PublishedAt?.ToString("MMMM dd, yyyy");
+                var videoMinutes = XmlConvert.ToTimeSpan(video.ContentDetails.Duration).ToString(@"mm");
+                if (videoMinutes[0] == '0') videoMinutes = videoMinutes.Remove(0, 1);
+                var videoSeconds = XmlConvert.ToTimeSpan(video.ContentDetails.Duration).ToString(@"ss");
 
                 var response = $"{video.Snippet.Title} | " +
-                               $"{video.Statistics.ViewCount} views";
-                if (video.Statistics.LikeCount != null) response += $" | {video.Statistics.LikeCount} likes";
-                if (video.Statistics.DislikeCount != null) response += $" | {video.Statistics.DislikeCount} dislikes";
-                response += $"\nUploaded on {videoDate} by {video.Snippet.ChannelTitle}\n" +
+                               $"{String.Format("{0:n0}", video.Statistics.ViewCount)} views | " +
+                               $"Duration: {videoMinutes} minutes {videoSeconds} seconds";
+
+                if (video.Statistics.LikeCount != null)
+                    response += $" | {String.Format("{0:n0}", video.Statistics.LikeCount)} likes";
+                if (video.Statistics.DislikeCount != null)
+                    response += $" | {String.Format("{0:n0}", video.Statistics.DislikeCount)} dislikes";
+
+                response += $" | Uploaded on {videoDate} by {video.Snippet.ChannelTitle}\n" +
                             $"https://youtu.be/{videoId}";
 
                 await context.Channel.SendMessageAsync(response);
@@ -69,7 +79,7 @@ namespace PlebBot.Helpers
                 var result = search.Items.FirstOrDefault();
                 videoId = result?.Id.VideoId;
             }
-            var videoRequest = _service.Videos.List("statistics, snippet");
+            var videoRequest = _service.Videos.List("statistics, snippet, contentDetails");
             videoRequest.Id = videoId;
             var videoResult = await videoRequest.ExecuteAsync();
             var video = videoResult.Items.FirstOrDefault();
