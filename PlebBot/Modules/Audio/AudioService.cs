c461b99949853ef507e2334b0cd1ca56e9a78c49
Angel Peltekov <ai.peltekov@gmail.com>
2018-02-20 11:43:09 +0200

    Revert "exlcuded player from release build"
    This reverts commit 3e00a9821516f4f6259caba5b9104e9ca5ad0bdb.

--- PlebBot/Modules/Audio/AudioService.cs
+++ PlebBot/Modules/Audio/AudioService.cs
@@ -1,91 +1,91 @@
-﻿//using System.Collections.Concurrent;
-//using System.Diagnostics;
-//using System.Runtime.InteropServices;
-//using System.Threading.Tasks;
-//using Discord;
-//using Discord.Audio;
-//using PlebBot.Helpers;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using PlebBot.Helpers;
 
-//namespace PlebBot.Modules
-//{
-//    public class AudioService
-//    {
-//        private readonly ConcurrentDictionary<ulong, IAudioClient> _connectedChannels = new ConcurrentDictionary<ulong, IAudioClient>();
+namespace PlebBot.Modules
+{
+    public class AudioService
+    {
+        private readonly ConcurrentDictionary<ulong, IAudioClient> _connectedChannels = new ConcurrentDictionary<ulong, IAudioClient>();
 
-//        public async Task JoinVoiceAsync(IGuild guild, IVoiceChannel channel)
-//        {
-//            IAudioClient client;
-//            if (_connectedChannels.TryGetValue(guild.Id, out client)) return;
-//            if (channel.Guild.Id != guild.Id) return;
-//            var audioClient = await channel.ConnectAsync();
-//            _connectedChannels.TryAdd(guild.Id, audioClient);
-//        }
+        public async Task JoinVoiceAsync(IGuild guild, IVoiceChannel channel)
+        {
+            IAudioClient client;
+            if (_connectedChannels.TryGetValue(guild.Id, out client)) return;
+            if (channel.Guild.Id != guild.Id) return;
+            var audioClient = await channel.ConnectAsync();
+            _connectedChannels.TryAdd(guild.Id, audioClient);
+        }
 
-//        public async Task LeaveVoiceAsync(IGuild guild)
-//        {
-//            IAudioClient client;
-//            if (_connectedChannels.TryRemove(guild.Id, out client))
-//                await client.StopAsync();
-//        }
+        public async Task LeaveVoiceAsync(IGuild guild)
+        {
+            IAudioClient client;
+            if (_connectedChannels.TryRemove(guild.Id, out client))
+                await client.StopAsync();
+        }
 
-//        public async Task StreamAudioAsync(IGuild guild, string path)
-//        {
-//            IAudioClient client;
-//            if (_connectedChannels.TryGetValue(guild.Id, out client))
-//            {
-//                using (var output = CreateStream(path).StandardOutput.BaseStream)
-//                using (var stream = client.CreatePCMStream(AudioApplication.Music))
-//                {
-//                    try { await output.CopyToAsync(stream); }
-//                    finally { await stream.FlushAsync(); }
-//                }
-//            }
-//        }
+        public async Task StreamAudioAsync(IGuild guild, string path)
+        {
+            IAudioClient client;
+            if (_connectedChannels.TryGetValue(guild.Id, out client))
+            {
+                using (var output = CreateStream(path).StandardOutput.BaseStream)
+                using (var stream = client.CreatePCMStream(AudioApplication.Music))
+                {
+                    try { await output.CopyToAsync(stream); }
+                    finally { await stream.FlushAsync(); }
+                }
+            }
+        }
 
-//        public Task<string> ConvertToAudioAsync(string path)
-//        {
-//            Process process;
-//            var audio = $"{path}.aac";
-//            //var args = $"-i \"{path}\" -q:a 0 -vn -ab 320k -ar 48000 -y \"{audio}\"";
-//            var args = $"-y -i {path} -vn -ar 44100 -ac 2 -b:a 320 -f aac {audio}";
-//            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-//            {
-//                process = Process.Start(new ProcessStartInfo()
-//                {
-//                    FileName = "/bin/bash",
-//                    Arguments = $"ffmpeg {args}",
-//                    UseShellExecute = false,
-//                    RedirectStandardOutput = true
-//                });
-//            }
-//            else
-//            {
-//                process = Process.Start(new ProcessStartInfo()
-//                {
-//                    FileName = "ffmpeg.exe",
-//                    Arguments = args,
-//                    UseShellExecute = false,
-//                    RedirectStandardOutput = true
-//                });
-//            }
-//            process?.WaitForExit();
-//            return Task.FromResult(audio);
-//        }
+        public Task<string> ConvertToAudioAsync(string path)
+        {
+            Process process;
+            var audio = $"{path}.aac";
+            //var args = $"-i \"{path}\" -q:a 0 -vn -ab 320k -ar 48000 -y \"{audio}\"";
+            var args = $"-y -i {path} -vn -ar 44100 -ac 2 -b:a 320 -f aac {audio}";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                process = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"ffmpeg {args}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                });
+            }
+            else
+            {
+                process = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "ffmpeg.exe",
+                    Arguments = args,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                });
+            }
+            process?.WaitForExit();
+            return Task.FromResult(audio);
+        }
 
-//        public Task<bool> CheckIfInVoice(IGuild guild)
-//        {
-//            return Task.FromResult(_connectedChannels.TryGetValue(guild.Id, out IAudioClient client));
-//        }
+        public Task<bool> CheckIfInVoice(IGuild guild)
+        {
+            return Task.FromResult(_connectedChannels.TryGetValue(guild.Id, out IAudioClient client));
+        }
 
-//        private Process CreateStream(string path)
-//        {
-//            return Process.Start(new ProcessStartInfo
-//            {
-//                FileName = "ffmpeg.exe",
-//                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
-//                UseShellExecute = false,
-//                RedirectStandardOutput = true
-//            });
-//        }
-//    }
-//}
+        private Process CreateStream(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg.exe",
+                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            });
+        }
+    }
+}
--- PlebBot/Modules/Audio/MusicPlayer.cs
+++ PlebBot/Modules/Audio/MusicPlayer.cs
@@ -1,126 +1,126 @@
-﻿//using System;
-//using System.Collections.Concurrent;
-//using System.Threading.Tasks;
-//using Discord;
-//using Discord.Commands;
-//using PlebBot.Helpers;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using PlebBot.Helpers;
 
-//namespace PlebBot.Modules
-//{
-//    public class MusicPlayer : ModuleBase<SocketCommandContext>
-//    {
-//        private readonly AudioService _audioService;
-//        private readonly YtService _ytService;
+namespace PlebBot.Modules
+{
+    public class MusicPlayer : ModuleBase<SocketCommandContext>
+    {
+        private readonly AudioService _audioService;
+        private readonly YtService _ytService;
 
-//        //allows different servers to have different playing queues
-//        private readonly ConcurrentDictionary<ulong, ConcurrentQueue<string>> _queues = new ConcurrentDictionary<ulong, ConcurrentQueue<string>>();
+        //allows different servers to have different playing queues
+        private readonly ConcurrentDictionary<ulong, ConcurrentQueue<string>> _queues = new ConcurrentDictionary<ulong, ConcurrentQueue<string>>();
 
-//        public MusicPlayer(AudioService audioService, YtService ytService)
-//        {
-//            this._audioService = audioService;
-//            this._ytService = ytService;
-//        }
+        public MusicPlayer(AudioService audioService, YtService ytService)
+        {
+            this._audioService = audioService;
+            this._ytService = ytService;
+        }
 
-//        [Command("play", RunMode = RunMode.Async)]
-//        [Alias("join")]
-//        [Summary("Get the bot in the voice chat")]
-//        public async Task Join()
-//        {
-//            await JoinAsync(Context);
-//            await Response.Success(
-//                Context, $"Joined your voice channel, {Context.User.Mention}. Now let's get the party going!");
-//        }
+        [Command("play", RunMode = RunMode.Async)]
+        [Alias("join")]
+        [Summary("Get the bot in the voice chat")]
+        public async Task Join()
+        {
+            await JoinAsync(Context);
+            await Response.Success(
+                Context, $"Joined your voice channel, {Context.User.Mention}. Now let's get the party going!");
+        }
 
-//        //TODO: parallel video download, audio conversion and download to improve the speed of the bot
-//        [Command("play", RunMode = RunMode.Async)]
-//        [Alias("queue", "q")]
-//        [Summary("Play a song")]
-//        public async Task PlayMusic([Remainder] [Summary("Name or link of the video you want to play")] string name)
-//        {
-//            if (!await _audioService.CheckIfInVoice(Context.Guild)) await JoinAsync(Context);
+        //TODO: parallel video download, audio conversion and download to improve the speed of the bot
+        [Command("play", RunMode = RunMode.Async)]
+        [Alias("queue", "q")]
+        [Summary("Play a song")]
+        public async Task PlayMusic([Remainder] [Summary("Name or link of the video you want to play")] string name)
+        {
+            if (!await _audioService.CheckIfInVoice(Context.Guild)) await JoinAsync(Context);
 
-//            try
-//            {
-//                var path = await _ytService.DownloadVideoAsync(name);
-//                var audio = $"{path}.mkv";
+            try
+            {
+                var path = await _ytService.DownloadVideoAsync(name);
+                var audio = $"{path}.mkv";
 
-//                //await EnqueueAsync(name);
+                //await EnqueueAsync(name);
 
-//                Parallel.Invoke(
-//                    async () =>
-//                    {
-//                        await SendNowPlaying(name);
-//                    },
-//                    async () =>
-//                    {
-//                        await _audioService.ConvertToAudioAsync(path);
-//                    });
+                Parallel.Invoke(
+                    async () =>
+                    {
+                        await SendNowPlaying(name);
+                    },
+                    async () =>
+                    {
+                        await _audioService.ConvertToAudioAsync(path);
+                    });
 
-//                await _audioService.StreamAudioAsync(Context.Guild, audio);
+                await _audioService.StreamAudioAsync(Context.Guild, audio);
 
 
-//                //await DequeueAsync();
-//            }
-//            catch (Exception ex)
-//            {
-//                await Response.Error(Context, "The requested video could not be played.");
-//            }
-//        }
+                //await DequeueAsync();
+            }
+            catch (Exception ex)
+            {
+                await Response.Error(Context, "The requested video could not be played.");
+            }
+        }
 
-//        [Command("stop", RunMode = RunMode.Async)]
-//        [Alias("leave")]
-//        [Summary("Stop playback")]
-//        public async Task LeaveVoice()
-//        {
-//            await _audioService.LeaveVoiceAsync(Context.Guild);
-//        }
+        [Command("stop", RunMode = RunMode.Async)]
+        [Alias("leave")]
+        [Summary("Stop playback")]
+        public async Task LeaveVoice()
+        {
+            await _audioService.LeaveVoiceAsync(Context.Guild);
+        }
 
-//        private Task EnqueueAsync(string name)
-//        {
-//            if (_queues.TryGetValue(Context.Guild.Id, out var queue))
-//                _queues.AddOrUpdate(Context.Guild.Id, queue, (k, v) =>
-//                {
-//                    v.Enqueue(name);
-//                    return v;
-//                });
-//            return Task.CompletedTask;
-//        }
+        private Task EnqueueAsync(string name)
+        {
+            if (_queues.TryGetValue(Context.Guild.Id, out var queue))
+                _queues.AddOrUpdate(Context.Guild.Id, queue, (k, v) =>
+                {
+                    v.Enqueue(name);
+                    return v;
+                });
+            return Task.CompletedTask;
+        }
 
-//        private Task DequeueAsync()
-//        {
-//            if (_queues.TryGetValue(Context.Guild.Id, out var queue))
-//                _queues.AddOrUpdate(Context.Guild.Id, queue, (k, v) =>
-//                {
-//                    v.TryDequeue(out var temp);
-//                    return v;
-//                });
-//            return Task.CompletedTask;
-//        }
+        private Task DequeueAsync()
+        {
+            if (_queues.TryGetValue(Context.Guild.Id, out var queue))
+                _queues.AddOrUpdate(Context.Guild.Id, queue, (k, v) =>
+                {
+                    v.TryDequeue(out var temp);
+                    return v;
+                });
+            return Task.CompletedTask;
+        }
 
-//        private async Task SendNowPlaying(string name)
-//        {
-//            var video = await _ytService.GetVideoAsync("", name);
-//            var uploadDate = video.Snippet.PublishedAt.Value.ToString("MMMM dd, yyyy");
-//            var response = new EmbedBuilder();
+        private async Task SendNowPlaying(string name)
+        {
+            var video = await _ytService.GetVideoAsync("", name);
+            var uploadDate = video.Snippet.PublishedAt.Value.ToString("MMMM dd, yyyy");
+            var response = new EmbedBuilder();
 
-//            response
-//                .WithUrl($"https://youtu.be/{video.Id}")
-//                .WithTitle("Now playing")
-//                .WithThumbnailUrl(video.Snippet.Thumbnails.Medium.Url)
-//                .AddField("Title:", video.Snippet.Title)
-//                .AddInlineField("Views", video.Statistics.ViewCount)
-//                .AddInlineField("Likes", video.Statistics.LikeCount)
-//                .AddInlineField("Dislikes", video.Statistics.DislikeCount)
-//                .WithFooter($"Uploaded by {video.Snippet.ChannelTitle} on {uploadDate}")
-//                .WithColor(Color.Red);
+            response
+                .WithUrl($"https://youtu.be/{video.Id}")
+                .WithTitle("Now playing")
+                .WithThumbnailUrl(video.Snippet.Thumbnails.Medium.Url)
+                .AddField("Title:", video.Snippet.Title)
+                .AddInlineField("Views", video.Statistics.ViewCount)
+                .AddInlineField("Likes", video.Statistics.LikeCount)
+                .AddInlineField("Dislikes", video.Statistics.DislikeCount)
+                .WithFooter($"Uploaded by {video.Snippet.ChannelTitle} on {uploadDate}")
+                .WithColor(Color.Red);
 
-//            await ReplyAsync("", false, response.Build());
-//        }
+            await ReplyAsync("", false, response.Build());
+        }
 
-//        private async Task JoinAsync(SocketCommandContext context)
-//        {
-//            await _audioService.JoinVoiceAsync(context.Guild, (context.User as IVoiceState)?.VoiceChannel);
-//            _queues.TryAdd(Context.Guild.Id, new ConcurrentQueue<string>());
-//        }
-//    }
-//}
+        private async Task JoinAsync(SocketCommandContext context)
+        {
+            await _audioService.JoinVoiceAsync(context.Guild, (context.User as IVoiceState)?.VoiceChannel);
+            _queues.TryAdd(Context.Guild.Id, new ConcurrentQueue<string>());
+        }
+    }
+}
