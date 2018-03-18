7cf5c6555bd20caf4511624bf88cd087fa21b543
Angel Peltekov <ai.peltekov@gmail.com>
2018-03-18 03:25:29 +0200

    typing state and exception logging

--- PlebBot/EventHandlers.cs
+++ PlebBot/EventHandlers.cs
@@ -14,7 +14,7 @@ namespace PlebBot
         {
             var message = messageParam as SocketUserMessage;
             if (message == null) return;
-            int argPos = 0;
+            var argPos = 0;
             var context = new SocketCommandContext(client, message);
 
             var condition = $"\"DiscordId\" = {context.Guild.Id}";
@@ -26,16 +26,34 @@ namespace PlebBot
             if (!(message.HasStringPrefix(prefix, ref argPos) ||
                   message.HasMentionPrefix(client.CurrentUser, ref argPos)) || message.Author.IsBot) return;
 
-            var result = await commands.ExecuteAsync(context, argPos, provider);
+            var searchResult = commands.Search(context, argPos);
+            if (!searchResult.IsSuccess) return;
 
-            Error(result, context);
-        }
+            foreach (var cmd in searchResult.Commands)
+            {
+                var preconditionResult = await cmd.CheckPreconditionsAsync(context);
+                if(!preconditionResult.IsSuccess) continue;
 
-        [System.Diagnostics.Conditional("DEBUG")]
-        private async void Error(IResult result, SocketCommandContext context)
-        {
-            if (!result.IsSuccess)
-                await context.Channel.SendMessageAsync(result.ErrorReason);
+                var parseResult = await cmd.ParseAsync(context, searchResult, preconditionResult);
+                if (!parseResult.IsSuccess && parseResult.Error != CommandError.MultipleMatches) continue;
+
+                var state = context.Channel.EnterTypingState();
+                await cmd.ExecuteAsync(context, parseResult, ConfigureServices(services));
+                state.Dispose();
+
+                commands.Log += msg =>
+                {
+                    if (!(msg.Exception is CommandException ex)) return Task.CompletedTask;
+
+                    var channel = client.GetChannel(417956085253668864) as ISocketMessageChannel;
+                    channel?.SendMessageAsync($"Fuck you <@164102776035475458> fix me!!!!1\n" +
+                                              $"{ex.Message}\n\n" +
+                                              $"{ex.InnerException.ToString()}\n");
+                    return Task.CompletedTask;
+                };
+
+                break;
+            }
         }
 
         private async Task HandleJoinGuildAsync(SocketGuild guild)
